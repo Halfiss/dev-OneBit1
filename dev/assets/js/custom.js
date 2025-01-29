dd657a2cfd55c176d14454cc6d28d391abe27199
Halfiss <71484923+Halfiss@users.noreply.github.com>
2025-01-29 12:33:35 +0200

    feat: fixed header, style: footer desktop done

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -6,6 +6,33 @@
   });
 
   
-const btnHistoryBack = document.querySelector('#history-back');
-const backHistory = ()=> window.history.back();
-btnHistoryBack.addEventListener('click', backHistory);
+// const btnHistoryBack = document.querySelector('#history-back');
+// const backHistory = ()=> window.history.back();
+// btnHistoryBack.addEventListener('click', backHistory);
+
+window.addEventListener("scroll", function() {
+  const header = document.querySelector("header");
+  if (window.scrollY > header.offsetTop) {
+      header.classList.add("fixed");
+  } else {
+      header.classList.remove("fixed");
+  }
+});
+
+
+const time = 11;
+let greeting;
+
+if (time < 4){
+  greeting = "Good Night";
+} else if (time < 12) {
+  greeting = "Good Morning";
+} else if (time < 16) {
+  greeting = "Good day";
+} else if (time < 24) {
+  greeting = "Good evening";
+} else {
+  greeting = "Wrong Time!"
+}
+
+console.log(greeting)
