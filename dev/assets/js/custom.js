2373b0bb1400abfcfde0c62ce1e6ad3c6e2302c9
Halfiss <71484923+Halfiss@users.noreply.github.com>
2025-02-12 23:36:13 +0200

    feat: creat slider

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,19 +1,41 @@
-  $(document).ready(function(){
-    $('[type="tel"]').mask('+38(000)-00-00-000');
-    $('#password').hideShowPassword({
-        innerToggle: true
-    });
-  });
+$(document).ready(function () {
+	$('[type="tel"]').mask("+38(000)-00-00-000");
+	$("#password").hideShowPassword({
+		innerToggle: true,
+	});
+});
 
 // const btnHistoryBack = document.querySelector('#history-back');
 // const backHistory = ()=> window.history.back();
 // btnHistoryBack.addEventListener('click', backHistory);
 
-window.addEventListener("scroll", function() {
-  const header = document.querySelector("header");
-  if (window.scrollY > header.offsetTop) {
-      header.classList.add("fixed");
-  } else {
-      header.classList.remove("fixed");
-  }
+window.addEventListener("scroll", function () {
+	const header = document.querySelector("header");
+	if (window.scrollY > header.offsetTop) {
+		header.classList.add("fixed");
+	} else {
+		header.classList.remove("fixed");
+	}
+});
+
+const swiper = new Swiper(".swiper", {
+	// Optional parameters
+	direction: "horizontal",
+	loop: true,
+	// If we need pagination
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
+
+	// Navigation arrows
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+
+	// And if we need scrollbar
+	scrollbar: {
+		el: ".swiper-scrollbar",
+	},
 });
